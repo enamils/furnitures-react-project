2cbd386d469e44a26099b1d6f42d59471c4e971f
Slimane GASSAMA <slimane.gassama@gmail.com>
2025-06-09 19:16:59 +0200

    feat: add button component Ui

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,26 +1,30 @@
 import * as React from "react";
+import Button from "./UI/Button.tsx";
+import heroImg from "../assets/images/couch.png";
 import classes from "./Hero.module.css";
 
-import heroImg from "../assets/images/couch.png"; // Adjust the path as necessary
-
-type heroProps = {
+type HeroProps = {
     title: string,
     text: string,
 }
 
-const Hero: React.FC<heroProps> = (props) => {
+const Hero: React.FC<HeroProps> = ({title, text}) => {
     return (
         <div className={classes.hero}>
             <div className="px-10 py-4">
                 <div className="container max-w-6xl md:flex mx-auto">
                     <div className="lg:flex w-full lg:gap-4">
                         <div className="w-full lg:w-5/12">
-                            <h1 className="text-white font-bold text-3xl xl:text-4xl 2xl:text-5xl mb-8">{props.title}</h1>
-                            <p>{props.text}</p>
+                            <h1 className="text-white font-bold text-3xl xl:text-5xl 2xl:text-6xl mb-8">{title}</h1>
+                            <p className="mb-6">{text}</p>
+                            <div>
+                                <Button as="link" to="/shop" label="Shop Now" className="mr-2" />
+                                <Button as="link" to="/contact" label="Explore" outlineButtonLink />
+                            </div>
                         </div>
                         <div className="w-full lg:w-7/12">
                             <div className="relative">
-                                <img src={heroImg} alt={props.title}/>
+                                <img src={heroImg} alt={title}/>
                             </div>
                         </div>
                     </div>
---
+++ src/components/UI/Button.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import { NavLink } from "react-router-dom";
+import classes from "./Button.module.css";
+
+type ButtonProps = {
+    as?: "button" | "link",
+    to?: string, // pour NavLink
+    label: string,
+    className?: string,
+    type?: "button" | "submit", // pour button
+    outlineButtonLink?: boolean,
+    onClick?: React.MouseEventHandler<HTMLButtonElement>,
+}
+
+const Button: React.FC<ButtonProps> = ({
+       as = "button",
+       to = "#",
+       label,
+       className = "",
+       type = "button",
+       outlineButtonLink = false,
+       onClick,
+       ...rest
+   }) => {
+
+    const cssButtonLink = [
+        classes.btn,
+        outlineButtonLink ? classes.btnOutline : classes.btnSecondary,
+        className
+    ].filter(Boolean).join(" ");
+
+    const cssButton = [
+        classes.btn,
+        classes.btnDark,
+        className
+    ].filter(Boolean).join(" ");
+
+    if (as === "link") {
+        return (
+            <NavLink to={to} className={cssButtonLink} {...rest}>
+                {label}
+            </NavLink>
+        );
+    }
+
+    return (
+        <button type={type} className={`${cssButton} cursor-pointer`} onClick={onClick} {...rest}>
+            {label}
+        </button>
+    );
+};
+
+export default Button;
