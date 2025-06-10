a0106f5cff1f80661a9d1220b0cd8d846e28093c
Slimane GASSAMA <slimane.gassama@gmail.com>
2025-06-10 22:40:28 +0200

    feat: templating of subscribe component with input

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,16 +3,20 @@ import sofaImg from "../assets/images/sofa.png";
 import Subscribe from "./Subscribe.tsx";
 
 const Footer: React.FC = () => {
-  return (
-      <footer role="contentinfo" className="bg-white pt-20 pb-20">
-          <div className="container max-w-6xl mx-auto relative">
-              <div className="absolute -top-[200px] right-0 z-1">
-                  <img src={sofaImg} alt="sofa" className="max-w-sm" />
-              </div>
-              <Subscribe />
-          </div>
-      </footer>
-  );
+    return (
+        <footer role="contentinfo" className="bg-white pt-20 pb-20">
+            <div className="container max-w-6xl mx-auto relative">
+                <div className="absolute -top-[200px] right-0 z-1">
+                    <img src={sofaImg} alt="sofa" className="max-w-sm"/>
+                </div>
+            </div>
+            <div className="px-10 py-4">
+                <div className="container max-w-6xl mx-auto relative">
+                    <Subscribe/>
+                </div>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
--- src/components/Subscribe.tsx
+++ src/components/Subscribe.tsx
@@ -1,12 +1,31 @@
 import * as React from "react";
 import Input from "./UI/Input.tsx";
+import SubscribeButton from "./UI/SubscribeButton.tsx";
+import imgLetterOutline from "../assets/images/envelope-outline.svg"
 
 const Subscribe: React.FC = () => {
     return (
         <div className=" w-full">
             <div className="w-full lg:w-8/12">
-                <p>Subscribe to Newsletters</p>
-                <Input id="test" placeholder="Enter your name" className="w-full" />
+                <div className="mb-10 mt-28 lg:mt-0 lg:mb-20 relative z-2">
+                    <p className="flex text-lg text-[color:var(--primary)] mb-2 font-medium">
+                        <span className="mr-1">
+                            <img src={imgLetterOutline} alt="Image"/>
+                        </span>
+                        Subscribe to Newsletter
+                    </p>
+                    <form className="flex flex-wrap gap-4 items-center w-full">
+                        <div className="flex-none w-auto">
+                            <Input id="test" placeholder="Enter your name" type="text"/>
+                        </div>
+                        <div className="flex-none w-auto">
+                            <Input id="test1" placeholder="Enter your email" type="email"/>
+                        </div>
+                        <div className="flex-none w-auto">
+                            <SubscribeButton/>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
