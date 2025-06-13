1588f5609b93f20fd0e67d959f9a85a75009457e
Slimane GASSAMA <slimane.gassama@gmail.com>
2025-06-13 22:40:47 +0200

    feat: add total cart items next to cart link in the header

--- src/components/Menu/ClientCart.tsx
+++ src/components/Menu/ClientCart.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
+import {NavLink} from "react-router-dom";
+import {useCart} from "../../hooks/useCart.ts";
 import clientImg from "../../assets/images/user.svg";
 import cartImg from "../../assets/images/cart.svg";
-import {NavLink} from "react-router-dom";
 
 const ClientCart: React.FC = () => {
+    const { cart } = useCart();
+
+    const totalCartItems = cart.reduce((total, product) => total + product.quantity, 0);
+
     return (
         <ul className="flex md:ml-10 items-center">
             <li className="mr-5">
@@ -12,8 +17,9 @@ const ClientCart: React.FC = () => {
                 </button>
             </li>
             <li>
-                <NavLink to="/cart" className="pl-2 pr-2">
+                <NavLink to="/cart" className="pl-2 pr-2 flex gap-2">
                     <img src={cartImg} alt="cart"/>
+                    <span className="text-white">({totalCartItems})</span>
                 </NavLink>
             </li>
         </ul>
