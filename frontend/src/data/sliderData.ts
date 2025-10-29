dc3013d782339a94f600a1fb9de2d48d245f6dc1
enamils <slimane.gassama@gmail.com>
2025-10-29 17:21:24 +0100

    refactor: remove unused titles from testimonial slides

--- frontend/src/data/sliderData.ts
+++ frontend/src/data/sliderData.ts
@@ -2,21 +2,18 @@ import personImg from "../assets/images/person-1.jpg";
 
 export const testimonialSlides = [
     {
-        title: "Ce que disent nos clients",
         content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
         image: personImg,
         name: "Maria Jones",
         position: "CEO, Co-Founder, XYZ Inc."
     },
     {
-        title: "Témoignages clients",
         content: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.",
         image: personImg,
         name: "John Smith",
         position: "Design Director, ABC Corp."
     },
     {
-        title: "Avis de nos partenaires",
         content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis.",
         image: personImg,
         name: "Sarah Wilson",
