b61910259f79f9d670a1b9b5d23c81a6f22420b6
enamils <slimane.gassama@gmail.com>
2025-10-30 18:54:25 +0100

    refactor: implement lazy loading for ProductItem and Post components in FeaturedProducts and LatestPosts

--- frontend/src/components/Products/FeaturedProducts.tsx
+++ frontend/src/components/Products/FeaturedProducts.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import ProductItem from "./ProductItem.tsx";
+const ProductItem = React.lazy(() => import("./ProductItem.tsx"));
 import {ProductSkeleton} from "../UI/Skeleton.tsx";
 import ErrorBlock from "../UI/ErrorBlock.tsx";
 import {useFeaturedProducts} from "../../hooks/useProducts.ts";
@@ -9,29 +9,6 @@ import type {Product} from "../../types/productType.ts";
 const FeaturedProducts: React.FC = () => {
     const {data: featuredProducts = [], isLoading, error} = useFeaturedProducts();
 
-    if (isLoading) {
-        return (
-            <div className="lg:flex lg:justify-between lg:gap-4 mb-16">
-                <div className="flex-2/4 mb-12 lg:mb-0">
-                    <h2 className="text-3xl text-[color:var(--dark)] mb-2 md:mb-6">Crafted with excellent material.</h2>
-                    <p className="mb-6">Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. </p>
-                    <Button as="link" label="Explore" to="/shop" darkButtonLink />
-                </div>
-                <ul className="grid justify-center md:grid-cols-3 gap-4">
-                    {[...Array(3)].map((_, i) => (
-                        <li key={i}>
-                            <ProductSkeleton />
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        );
-    }
-
-    if (error) {
-        return <ErrorBlock title="An error has occurred" message="Unable to load featured products." />
-    }
-
     return (
         <div className="lg:flex lg:justify-between lg:gap-4 mb-16">
             <div className="flex-2/4 mb-12 lg:mb-0">
@@ -39,11 +16,36 @@ const FeaturedProducts: React.FC = () => {
                 <p className="mb-6">Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. </p>
                 <Button as="link" label="Explore" to="/shop" darkButtonLink />
             </div>
-            <ul className="grid justify-center sm:grid-cols-3 gap-4">
-                {featuredProducts.map((product: Product) => (
-                    <ProductItem key={product.id} product={product}/>
-                ))}
-            </ul>
+
+            <div className="w-full">
+                {error ? (
+                    <ErrorBlock title="An error has occurred" message="Unable to load featured products." />
+                ) : (
+                    <ul className="grid justify-center sm:grid-cols-3 gap-4">
+                        {isLoading ? (
+                            [...Array(3)].map((_, i) => (
+                                <li key={i}>
+                                    <ProductSkeleton />
+                                </li>
+                            ))
+                        ) : (
+                            <React.Suspense fallback={
+                                <>
+                                    {[...Array(3)].map((_, i) => (
+                                        <li key={i}>
+                                            <ProductSkeleton />
+                                        </li>
+                                    ))}
+                                </>
+                            }>
+                                {featuredProducts.map((product: Product) => (
+                                    <ProductItem key={product.id} product={product} />
+                                ))}
+                            </React.Suspense>
+                        )}
+                    </ul>
+                )}
+            </div>
         </div>
     );
 }
